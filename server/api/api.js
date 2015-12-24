e90687a56a717397e086cbf2f5081c49c2c4fc20
SirSeim <sirseim@gmail.com>
2015-12-24 14:12:17 -0700

    Basic folder layout of pages and api & prelimiary file serving

---
+++ server/api/api.js
@@ -0,0 +1,7 @@
+
+
+var Api = {
+
+};
+
+module.exports = Api;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,18 @@
+var Api = require("../api/api.js")
+
 var Routes = [
     {
-        method: 'GET',
-        path: '/hello',
+        method: "GET",
+        path: "/hello",
         handler: function (request, reply) {
             reply({"hello":"Welcome to the Service API!"}).code(200);
+        }
+    },
+    {
+        path:"/{path*}",
+        method: "GET",
+        handler: function(request, reply) {
+            reply.file("./pages/"+request.params.path);
         }
     }
 ];
