fa68607be18242b8d664fd1e5e9595c7a1f1c80a
SirSeim <sirseim@gmail.com>
2016-01-02 09:59:08 -0800

    login page progress, success alert on redirect back to /

--- parse_cloud/cloud/app.js
+++ parse_cloud/cloud/app.js
@@ -1,27 +1,61 @@
 
 // These two lines are required to initialize Express in Cloud Code.
- express = require('express');
- app = express();
+var express = require('express');
+var app = express();
+var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+var parseExpressCookieSession = require('parse-express-cookie-session');
 
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
+app.use(express.cookieParser('6i=p#d#z()oeko24g2=f%t4-c$q6_q195*eu3ank!v)3qhs9e='));
+app.use(parseExpressHttpsRedirect());
+app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
     res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
+app.post('/hello', function(req, res) {
+    res.render('hello', { message: req.body.message });
+});
+
+
 app.get('/', function(req, res) {
+    console.log('req.query.login: ' + req.query.login);
+    var alerts = [];
+    if(req.query.login == 'success') {
+        alerts.push({
+            type: 'success',
+            message: 'Login Successful'
+        });
+    }
     res.render('index', {
+        alerts: alerts,
+        leftMessage: 'Congrats bitches, you did it',
+        rightMessage: 'But seriously, WTF!?'
+    });
+});
+app.get('/index', function(req,res) {
+    var alerts = [];
+    res.render('index', {
+        alerts: alerts,
         leftMessage: 'Congrats bitches, you did it',
         rightMessage: 'But seriously, WTF!?'
     });
 });
 
-app.post('/hello', function(req, res) {
-    res.render('hello', { message: req.body.message });
+app.get('/login', function(req, res) {
+    console.log('login page requested')
+    res.render('login');
+});
+
+app.post('/login', function(req, res) {
+    // console.log(req);
+    res.redirect('/?login=success')
+    // Parse.User.logIn(req.body.usename, req.body.password)
 });
 
 // // Example reading from the request query string of an HTTP get request.
