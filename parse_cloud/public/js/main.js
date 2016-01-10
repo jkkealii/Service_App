045eb4924beecee97b89564a0337cf1b94a1f9c9
Josh <jkkealii@gmail.com>
2016-01-09 17:31:06 -0800

    almost done with the navbar dropdown stuff. irritating that i can't figure out some css stuff though. like line 376 where i can't figure out why there is now an underline when hovering over the icon at the top of the page. tried to fix it, even after taking out the text-transformation property from 'a' nothing happened. also line 291 where i can't change or take out the border once the on class is applied to the dropdown menu

--- parse_cloud/public/js/main.js
+++ parse_cloud/public/js/main.js
@@ -31,8 +31,10 @@
         var navHeight = $(window).height() - 100;
         if ($(window).scrollTop() > navHeight) {
             $('.navbar-default').addClass('on');
+            $('.dropdown-menu').addClass('on');
         } else {
             $('.navbar-default').removeClass('on');
+            $('.dropdown-menu').removeClass('on');
         }
     });
 
@@ -40,82 +42,6 @@
         target: '.navbar-default',
         offset: 80
     })
-
-    $(document).ready(function() {
-        $("#team").owlCarousel({
-     
-            navigation : false, // Show next and prev buttons
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            autoHeight : true,
-            itemsCustom : [
-                [0, 1],
-                [450, 2],
-                [600, 2],
-                [700, 2],
-                [1000, 4],
-                [1200, 4],
-                [1400, 4],
-                [1600, 4]
-            ],
-        });
-
-        $("#clients").owlCarousel({
-     
-            navigation : false, // Show next and prev buttons
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            autoHeight : true,
-            itemsCustom : [
-                [0, 1],
-                [450, 2],
-                [600, 2],
-                [700, 2],
-                [1000, 4],
-                [1200, 5],
-                [1400, 5],
-                [1600, 5]
-            ],
-        });
-
-        $("#testimonial").owlCarousel({
-            navigation : false, // Show next and prev buttons
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            singleItem:true
-        });
-
-    });
-
-    /*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('#lightbox');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
 
 
 
