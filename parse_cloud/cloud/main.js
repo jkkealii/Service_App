9eb12dd01c7da16fb65a848e8512156a94b753c1
Edward Seim <sirseim@gmail.com>
2016-01-26 12:03:23 -0800

    getting started on background job for hours processing

--- parse_cloud/cloud/main.js
+++ parse_cloud/cloud/main.js
@@ -1,25 +1,33 @@
 require('cloud/app.js');
 
+
+// get all users, make an object with the ids as keys for quick access
+// get all events and their drivers and memebers
+// add hours to users
 Parse.Cloud.job("userHoursUpdate", function(request, status) {
-    // Set up to modify user data
     Parse.Cloud.useMasterKey();
     var counter = 0;
-    // Query for all users
+    var usersObj = {};
+
     var query = new Parse.Query(Parse.User);
-    query.each(function(user) {
-        // Update to plan value passed in
-        user.set("plan", request.params.plan);
-        if (counter % 100 === 0) {
-            // Set the  job's progress status
-            status.message(counter + " users processed.");
-        }
-        counter += 1;
-        return user.save();
-    }).then(function() {
-        // Set the job's success status
-        status.success("Migration completed successfully.");
-    }, function(error) {
-        // Set the job's error status
-        status.error("Uh oh, something went wrong.");
+    query.find().then(function(listUsers) {
+
     });
+
+    // query.each(function(user) {
+    //     // Update to plan value passed in
+    //     user.set("plan", request.params.plan);
+    //     if (counter % 100 === 0) {
+    //         // Set the  job's progress status
+    //         status.message(counter + " users processed.");
+    //     }
+    //     counter += 1;
+    //     return user.save();
+    // }).then(function() {
+    //     // Set the job's success status
+    //     status.success("Migration completed successfully.");
+    // }, function(error) {
+    //     // Set the job's error status
+    //     status.error("Uh oh, something went wrong.");
+    // });
 });
