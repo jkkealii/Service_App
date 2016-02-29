e8bd5723946b41439c947fd364a44aa5d3a9e28f
SirSeim <sirseim@gmail.com>
2016-02-29 15:47:14 -0800

    User pages now have their relevant events loaded
    I bet it bugged everyone how the table is just always NOT populated.

---
+++ parse_cloud/public/js/user.js
@@ -0,0 +1,56 @@
+$(function () {
+    var newRow = function(parseObject) {
+        var campusText = (parseObject.isOnCampus ? 'On Campus' : 'Off Campus');
+        return '<tr><td>' + '<a href="/events/' + parseObject.objectId + '">' + parseObject.name + '</a>' + '</td>' + 
+            '<td>' + moment(parseObject.startDateTime.iso).format("MMM D, YYYY, h:mm a") + 
+            '</td>' + '<td><a href="https://maps.google.com/?q=' + parseObject.location +
+            '">' + parseObject.location + '</a></td>' + '<td>' + campusText + '</td> ' +
+            '<td><a href="/events/' + parseObject.objectId + '" class="btn buton btn-default btn-sm">Details</a></td></tr>';
+    };
+
+    var userId = $('#user').data('user-id');
+    $('table tbody').empty();
+    $.ajax({
+        method: 'GET',
+        url: '/users/' + userId + '/events',
+    }).then(function(data, textStatus, jqXHR) {
+        console.log('done');
+        console.log(data);
+        $.each(data.events, function(index, value) {
+            $('table tbody').append($(newRow(value)).data('start-date-time', moment(value.startDateTime.iso)));
+        });
+        refreshShownEvents();
+        $('#show-past-events').prop('disabled', false);
+    }, function(jqXHR, textStatus, error) {
+        console.log(error);
+        console.log(textStatus);
+    });
+
+    var refreshShownEvents = function() {
+        var showPastEvents = $('#show-past-events').is(':checked');
+        $('table tbody tr').each(function(index, element) {
+            if (!showPastEvents) {
+                // console.log('do not show past events');
+                if ($(element).data('start-date-time').isSameOrAfter(moment(), 'day')) {
+                    // console.log(element);
+                    // console.log('show');
+                    $(element).show();
+                    $(element).data('searchable', true);
+                } else {
+                    // console.log(element);
+                    // console.log('hide');
+                    $(element).data('searchable', false);
+                    $(element).hide();
+                }
+            } else {
+                // console.log('show past events');
+                $(element).show();
+                $(element).data('searchable', true);
+            }
+        });
+    };
+
+    $('#show-past-events').parent().parent().click(function() {
+        refreshShownEvents();
+    });
+});
