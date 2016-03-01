375dd0a6b6a453a23905a9db586818ed5347dec5
SirSeim <sirseim@gmail.com>
2016-02-29 19:46:34 -0800

    Working on making direct actions on events
    The dropdown is coming up, its just cut off for the lower events

--- parse_cloud/public/js/events.js
+++ parse_cloud/public/js/events.js
@@ -1,11 +1,27 @@
 $(function () {
-    var newRow = function(parseObject) {
+    var actions = function(objectId, auth) {
+        if (auth) {
+            return '<div class="btn-group">' +
+                        '<a href="/events/' + objectId + '" class="btn buton btn-default btn-sm">Details</a>' +
+                        '<button type="button" class="btn fk-buton btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span><span class="sr-only">Toggle Dropdown</span></button>' +
+                        '<ul class="dropdown-menu">' +
+                            '<li><a href="#">Duplicate</a></li>' +
+                            '<li><a href="#">Edit</a></li>' +
+                            '<li><a href="#">Delete</a></li>' +
+                        '</ul>' +
+                    '</div>';
+        } else {
+            return '<a href="/events/' + objectId + '" class="btn buton btn-default btn-sm">Details</a>';
+        }
+    };
+
+    var newRow = function(parseObject, auth) {
         var campusText = (parseObject.isOnCampus ? 'On Campus' : 'Off Campus');
         return '<tr><td>' + '<a href="/events/' + parseObject.objectId + '">' + parseObject.name + '</a>' + '</td>' + 
             '<td>' + moment(parseObject.startDateTime.iso).format("MMM D, YYYY, h:mm a") + 
             '</td>' + '<td><a href="https://maps.google.com/?q=' + parseObject.location +
             '">' + parseObject.location + '</a></td>' + '<td>' + campusText + '</td> ' +
-            '<td><a href="/events/' + parseObject.objectId + '" class="btn buton btn-default btn-sm">Details</a></td></tr>';
+            '<td>' + actions(parseObject.objectId, auth) + '</td></tr>';
     };
 
     $('table tbody').empty();
@@ -15,8 +31,12 @@ $(function () {
     }).then(function(data, textStatus, jqXHR) {
         console.log('done');
         console.log(data);
+        if (typeof data.auth === 'undefined') {
+            data.auth = 'true';
+        }
+        data.auth = (data.auth === 'true');
         $.each(data.objects, function(index, value) {
-            $('table tbody').append($(newRow(value)).data('start-date-time', moment(value.startDateTime.iso)));
+            $('table tbody').append($(newRow(value, data.auth)).data('start-date-time', moment(value.startDateTime.iso)));
         });
         refreshShownEvents();
         $('#show-past-events').prop('disabled', false);
