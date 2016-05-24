5901369e470dbbac061af4a09c981bae92c449fd
SirSeim <sirseim@gmail.com>
2016-05-24 08:32:09 -0700

    Server now runs
    Last bit of console control implemented

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi');
 var Config = require('config');
 var Path = require('path');
+var Vision = require('vision');
 
 var setup = Config.get('Node-Server');
 var routes = require(Path.join(__dirname, 'routes/routes.js'));
@@ -8,25 +9,39 @@ var routes = require(Path.join(__dirname, 'routes/routes.js'));
 var showServerRunInfo = function () {
     console.log("Server started on %s:%s", setup.host, setup.port);
 };
+
 var options = {
-    opsInterval: 1000,
-    reporters: [{
-        reporter: require('good-console'),
-        events: { log: '*', response: '*' }
-    }],
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        console: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{
+                log: '*',
+                response: '*'
+            }]
+        }, {
+            module: 'good-console'
+        }, 'stdout']
+    },
 };
+
 var templates = {
     engines: {
         html: require('nunjucks-hapi')
     },
     path: Path.join(__dirname, 'static/templates')
 };
+
 var registerCallback = function (err) {
     if (err) {
         console.error(err);
         throw err; // something bad happened loading the plugin
     }
 };
+
 var server = function (setup, routes, options, registerCallback, templates) {
     var hapi = new Hapi.Server();
     
@@ -35,9 +50,16 @@ var server = function (setup, routes, options, registerCallback, templates) {
         port: setup.port
     });
 
-    hapi.route(routes);
+    hapi.register(Vision, function (err) {
+        hapi.views({
+            engines: {
+                html: require('nunjucks-hapi')
+            },
+            path: Path.join(__dirname, 'static/templates')
+        });
 
-    hapi.views(templates);
+        hapi.route(routes);
+    });
 
     if (setup.logToConsole) {
         hapi.register({
