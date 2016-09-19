a3afabaae2bccd4be217f45b374b907551101d82
SirSeim <sirseim@gmail.com>
2016-09-19 08:17:20 -0700

    Corrected http codes for responses

--- api/respond.js
+++ api/respond.js
@@ -6,11 +6,12 @@ var respond = {
         }).code(500);
     },
     returnEvents: function (res, events) {
+        var code = (events.length) ? 200 : 404;
         res({
-            statusCode: 200,
+            statusCode: code,
             message: (events.length) ? "Success finding events!" : "There are no events currently!",
             events: events
-        }).code(200);
+        }).code(code);
     },
     failedToCreateEvent: function (res) {
         res({
@@ -20,10 +21,10 @@ var respond = {
     },
     createdEvent: function (res, result) {
         res({
-            statusCode: 200,
+            statusCode: 201,
             message: "Success creating event!",
             result: result
-        }).code(200);
+        }).code(201);
     },
     failedToDeleteEvent: function (res) {
         res({
@@ -32,11 +33,12 @@ var respond = {
         }).code(500);
     },
     deletedEvent: function (res, result) {
+        var code = (result.result.n) ? 200 : 404;
         res({
-            statusCode: 200,
+            statusCode: code,
             message: (result.result.n) ? "Success deleting event!" : "No event found to delete!",
             result: result.result
-        }).code(200);
+        }).code(code);
     },
     failedToGetEvent: function (res) {
         res({
@@ -45,11 +47,12 @@ var respond = {
         }).code(500);
     },
     gotEvent: function (res, event) {
+        var code = (result.result.n) ? 200 : 404;
         res({
-            statusCode: 200,
+            statusCode: code,
             message: (event) ? "Success finding event!" : "Event not found!",
             event: event
-        }).code(200);
+        }).code(code);
     }
 };
 
