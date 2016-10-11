f7c2cc8456e39fb18f9abb01fee5daebbc626fb3
Edward Seim <sirseim@gmail.com>
2016-10-11 16:52:40 -0700

    Trying to make `schedule` dependent on mongo

--- api/schedule.js
+++ api/schedule.js
@@ -3,24 +3,30 @@ var schedule = require('node-schedule');
 var Api = require(Path.join(__dirname, 'api.js'));
 
 module.exports.register = function (server, options, next) {
-    var jobs = [
-        schedule.scheduleJob('0,10,20,30,40,50 * * * *', function () {
-            server.log(['schedule', 'info'], "Starting hours job");
-            var func = function (obj) {
-                if (obj.statusCode === 200) {
-                    server.log(['schedule', 'info'], "Hours job successful!");
-                } else if (obj.statusCode === 500) {
-                    server.log(['schedule', 'error'], "Hours job failed!");
-                    server.log(['schedule', 'error'], obj.error);
-                }
-            };
-            func.code = function () {};
-            Api.calculateHours({
-                mongo: server.mongo
-            }, func);
-        })
-    ];
-    server.decorate('server', 'schedule', jobs);
+    server.dependency('mongo-connection', function (server, next) {
+        var jobs = [
+            schedule.scheduleJob('* * * * *', function () {
+                server.log(['schedule', 'info'], "Starting hours job");
+                var code = function () {};
+                var func = function (obj) {
+                    if (obj.statusCode === 200) {
+                        server.log(['schedule', 'info'], "Hours job successful!");
+                    } else if (obj.statusCode === 500) {
+                        server.log(['schedule', 'error'], "Hours job failed!");
+                        server.log(['schedule', 'error'], obj.error);
+                    }
+                    return {code};
+                };
+                console.log(func);
+                Api.calculateHours({
+                    mongo: options.mongo
+                }, func);
+            })
+        ];
+        console.log(server.expose);
+        server.decorate('server', 'schedule', jobs);
+        next();
+    });
     next();
 };
 
