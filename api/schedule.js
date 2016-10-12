0f60a21a6a9d09d89cbfaec532d7d4cdfc6cd1b9
Edward Seim <sirseim@gmail.com>
2016-10-11 17:25:43 -0700

    Temporary work around for scheduling
    technically should use mongo-connection plugin, but for now is just opening its own connection

--- api/schedule.js
+++ api/schedule.js
@@ -2,8 +2,17 @@ var Path = require('path');
 var schedule = require('node-schedule');
 var Api = require(Path.join(__dirname, 'api.js'));
 
+var Mongo = require('mongodb');
+var MongoClient = Mongo.MongoClient;
+var ObjectID = Mongo.ObjectID;
+
 module.exports.register = function (server, options, next) {
-    server.dependency('mongo-connection', function (server, next) {
+    MongoClient.connect(process.env.DATABASE_URL, function (err, db) {
+        if (err) {
+            server.log(['schedule', 'error'], err);
+            return next(err);
+        }
+        server.log(['schedule', 'error'], 'Connected to ' + process.env.DATABASE_URL);
         var jobs = [
             schedule.scheduleJob('* * * * *', function () {
                 server.log(['schedule', 'info'], "Starting hours job");
@@ -17,20 +26,48 @@ module.exports.register = function (server, options, next) {
                     }
                     return {code};
                 };
-                console.log(func);
                 Api.calculateHours({
-                    mongo: options.mongo
+                    mongo: {
+                        ObjectID: ObjectID,
+                        db: db
+                    }
                 }, func);
             })
         ];
-        console.log(server.expose);
         server.decorate('server', 'schedule', jobs);
+
         next();
     });
-    next();
+
+    // server.dependency(['mongo-connection'], function (server, next) {
+    //     server.log(['schedule', 'info'], "sd");
+    //     var jobs = [
+    //         schedule.scheduleJob('* * * * *', function () {
+    //             server.log(['schedule', 'info'], "Starting hours job");
+    //             var code = function () {};
+    //             var func = function (obj) {
+    //                 if (obj.statusCode === 200) {
+    //                     server.log(['schedule', 'info'], "Hours job successful!");
+    //                 } else if (obj.statusCode === 500) {
+    //                     server.log(['schedule', 'error'], "Hours job failed!");
+    //                     server.log(['schedule', 'error'], obj.error);
+    //                 }
+    //                 return {code};
+    //             };
+    //             console.log(func);
+    //             Api.calculateHours({
+    //                 mongo: server.mongo
+    //             }, func);
+    //         })
+    //     ];
+    //     server.decorate('server', 'schedule', jobs);
+    //     next();
+    // });
+    // next();
 };
 
 module.exports.register.attributes = {
     name: "schedule",
-    version: "0.0.0"
+    version: "0.0.0",
+    dependencies: ['mongo-connection']
 };
